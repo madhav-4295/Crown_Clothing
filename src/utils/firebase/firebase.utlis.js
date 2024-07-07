16853389d8594f40cf4212ef6e75da80b1fa89f8
Madhav Mishra <maddy4295@gmail.com>
2024-07-06 22:37:08 -0400

    directory links added

--- src/utils/firebase/firebase.utlis.js
+++ src/utils/firebase/firebase.utlis.js
@@ -49,7 +49,6 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) =>{
     })
     await batch.commit()
 
-    console.log("done")
 }
 
 export const getCategoriesAndDocuments = async () => {
